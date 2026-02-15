2bd8f7f9b25dcee97126c2b33325448743481923
Diego Mercado <diegomercado2002@gmail.com>
2026-02-15 18:59:21 +0100

    feat: create and display posts and comments

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+
+const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
+    setContent("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <p>Create Comment</p>
+          <input
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className={"form-control"}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentCreate;
